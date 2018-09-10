e23943b1824b93080e43c37fb6df4d76d5d51218
Jonah <jonah.jaruta.2019@student.lhschools.org>
2018-09-10 08:07:04 -0400

    Got to point of asking for userWeight

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,27 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    //declare
+        Scanner keyboard;
+        String userName;
+        String userWeight;
+        //initializing
+        keyboard = new Scanner (System.in);
+
+        System.out.println("Hi, we're gonna calculate your BMI. Lets start with your name, what is it?");
+        userName = keyboard.nextLine();
+        System.out.println("Hey " +userName+ ", to find your BMI I need to ask a few questions.");
+        System.out.println("For starters, could you tell me what your weight is?");
+        userWeight = keyboard.nextLine();
+
+
+
+
+
+
     }
 }
