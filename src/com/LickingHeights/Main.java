e161c820abf6e6f94644687e529e9580d2e9eb94
Jonah <jonah.jaruta.2019@student.lhschools.org>
2018-09-11 07:47:06 -0400

    Finished the Code

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -8,15 +8,26 @@ public class Main {
 	    //declare
         Scanner keyboard;
         String userName;
-        String userWeight;
+        float userWeight;
+        float userHeight;
+        float total;
+        float numberOne;
+
         //initializing
         keyboard = new Scanner (System.in);
+        numberOne = 703;
 
         System.out.println("Hi, we're gonna calculate your BMI. Lets start with your name, what is it?");
         userName = keyboard.nextLine();
         System.out.println("Hey " +userName+ ", to find your BMI I need to ask a few questions.");
         System.out.println("For starters, could you tell me what your weight is?");
-        userWeight = keyboard.nextLine();
+        userWeight = keyboard.nextFloat();
+        System.out.println("Next, could I get your height in inches?");
+        userHeight = keyboard.nextFloat();
+        System.out.println("Thanks! With that information I will calculate your BMI");
+        total = userWeight/(userHeight*userHeight)*numberOne;
+        System.out.println("Alright "+userName+ " your BMI is "+total+ ".");
+
 
 
 
